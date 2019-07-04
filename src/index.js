07f4ee62ff126fe8c67b85f0b85265afaaba51e3
camel113 <adrienbigler@gmail.com>
2019-07-04 18:22:04 +0200

    Add transition between pages

--- src/index.js
+++ src/index.js
@@ -8,8 +8,18 @@ import Home from "./components/HomePage/Home" ;
 import Selfie from "./components/Game/Selfie" ;
 import ImageDescription from "./components/Game/ImageDescription/ImageDescription" ;
 import registerServiceWorker from "./registerServiceWorker";
+import { CSSTransition } from 'react-transition-group'
+
+const routes = [
+  { path: '/', name: 'Home', Component: Home },
+  { path: '/about', name: 'About', Component: About },
+  { path: '/game', name: 'Game', Component: Game },
+  { path: '/selfie', name: 'Selfie', Component: Selfie },
+  { path: '/image', name: 'ImageDescription', Component: ImageDescription },
+]
 
 export default class App extends Component {
+
 
   render() {
 
@@ -18,15 +28,26 @@ export default class App extends Component {
     const basename = publicUrl !== '' && publicUrl !== '/' ? new URL(publicUrl).pathname : '';
 
     return (
-    <Router basename={basename}>
-      <div id="App">
-        <Route exact path={'/'} component={Home} />
-        <Route exact path={'/about'} component={About} />
-        <Route exact path={'/game'} component={Game} />
-        <Route exact path={'/selfie'} component={Selfie} />
-        <Route exact path={'/image'} component={ImageDescription} />
-      </div>
-    </Router>
+      <Router basename={basename}>
+        <div id="App">
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <div className="page">
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+        </div>
+      </Router>
     );
   }
 }
