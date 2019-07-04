56210eb69eaf379eefe6e0f9785e71fc4f5794ae
camel113 <adrienbigler@gmail.com>
2019-07-04 14:29:15 +0200

    Cleanup code

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -25,7 +25,7 @@ function Game (props) {
   const nbTiles = props.location.state.nbTiles;
   const tileWidth = (w/(nbTiles*nbTiles))*props.location.state.tilesWidth
   coord.forEach((item,index) => {
-    rectangles.push(<Rectangle key={index} color="transparent" bounds={[[(nbTiles-item.x)*tileWidth,(item.y)*tileWidth],[((nbTiles-item.x)-1)*tileWidth,((item.y)+1)*tileWidth]]} onClick={(e) => {console.log(item.thumbRef); props.history.push('/image',{imageName:item.thumbRef})}}/>)
+    rectangles.push(<Rectangle key={index} color="transparent" bounds={[[(nbTiles-item.x)*tileWidth,(item.y)*tileWidth],[((nbTiles-item.x)-1)*tileWidth,((item.y)+1)*tileWidth]]} onClick={(e) => {props.history.push('/image',{imageName:item.thumbRef})}}/>)
   })
   const [zoomLevel, setZoomLevel] = useState([[w/2,h/2], [w/2,h/2]]);
 
@@ -42,13 +42,12 @@ function Game (props) {
         )}
       </nav>
       <div style={{marginTop:`${props.location.state.paddingTop}px`}}>
-        <Map onZoom={(e) => console.log(e)} crs={L.CRS.Simple} boundsOptions={[[0,0], [w,h]]} maxZoom={4} zoomControl={false} attributionControl={false} bounds={zoomLevel} maxBounds={[[0,0], [w,h]]} maxBoundsViscosity={1.0} style={{width: `${w}px`, height:`${w}px`}}>
+        <Map crs={L.CRS.Simple} boundsOptions={[[0,0], [w,h]]} maxZoom={4} zoomControl={false} attributionControl={false} bounds={zoomLevel} maxBounds={[[0,0], [w,h]]} maxBoundsViscosity={1.0} style={{width: `${w}px`, height:`${w}px`}}>
           <ImageOverlay
             url={props.location.state.mosaicFileUrl}
             bounds={[[0,0], [w,h]]}
             onLoad={() => {
               setTimeout(() => {
-                console.log('setZoomLevel')
                 setZoomLevel([[0,0], [w,h]])
               },400)
             }}
